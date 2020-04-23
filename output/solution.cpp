ef44af382207ded4a91c8fcacc54d4f8cd529e10
cunbidun <duy.tht.pham@gmail.com>
2020-04-23 21:36:57 +0700

    Initial commit

--- output/solution.cpp
+++ output/solution.cpp
@@ -14,11 +14,14 @@ typedef pair<int, int> ii;
 typedef pair<ii, int> iii;
 typedef vector<ii> vii;
 
-const int N = 1e5 + 1;
+const int N = 2e5 + 1;
 const int INF = 2e9;
 
+ll n,k,x;
+ll a[N];
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+    cin >> n >> k >> x;
     
 }

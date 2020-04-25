e5599572ac2b4586635adffb9e59e5bb82bfafb1
cunbidun <duy.tht.pham@gmail.com>
2020-04-25 11:32:30 +0700

    Test Interactor

---
+++ Task/solution.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(), (v).end()
+#define rf(i, a, b) for (int(i) = (a); (i) <= (b); (i)++)
+#define rb(i, b, a) for (int(i) = (b); (i) >= (a); (i)--)
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef pair<ii, int> iii;
+typedef vector<ii> vii;
+
+const int N = 1e5 + 1;
+const int INF = 2e9;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int l = 1, r = 1e9;
+    while (l != r) {
+        int m = (l + r) >> 1;
+        cout << "? " <<  m << endl;
+        int ans;
+        cin >> ans;
+        if(ans == 0) 
+            return cout << "! " << m << endl, 0;
+        if(ans == 1) 
+            l = m; 
+        else r = m;
+    }
+}

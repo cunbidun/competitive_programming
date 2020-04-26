30e4d8cfbfa51bea8a5fc7c8ea66063055427a7c
Duy <cunbidun@gmail.com>
2020-04-26 15:16:04 +0700

    move slow solution to seperate files

--- Todo/CP.md
+++ Todo/CP.md
@@ -54,6 +54,5 @@ Format Code: Ctrl + Alt + L
 2. Implement interactor checker.
 3. Silent testing (not print test)
 4. Speed testing phase
-5. Slow solution in seperate file? (convenient for coding, but may reduce performance)
-    Not really
+5. ~~Slow solution in seperate file? (convenient for coding, but may reduce performance)~~
 6. Rewrite script?
--- output/solution.cpp
+++ output/solution.cpp
@@ -2,9 +2,9 @@
 
 #define pb push_back
 #define sz(v) ((int)(v).size())
-#define all(v) (v).begin(),(v).end()
-#define rf(i, a, b) for(int (i) = (a); (i) <= (b); (i)++)
-#define rb(i, b, a) for(int (i) = (b); (i) >= (a); (i)--)
+#define all(v) (v).begin(), (v).end()
+#define rf(i, a, b) for (int(i) = (a); (i) <= (b); (i)++)
+#define rb(i, b, a) for (int(i) = (b); (i) >= (a); (i)--)
 
 using namespace std;
 
@@ -17,8 +17,12 @@ typedef vector<ii> vii;
 const int N = 1e5 + 1;
 const int INF = 2e9;
 
-int main(){
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
+    int ans = 0;
+    int n;
+    cin >> n;
+    rf(i, 1, n) rf(j, 1, n) ans += i * j;
+    cout << ans << "\n";
 }

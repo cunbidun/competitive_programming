74a3bac604985ebf0b64df300bb1707a2f1f517e
Duy Pham <cunbidun@gmail.com>
2020-11-10 22:13:38 +0700

    Complete document for the first version

--- Script/precompiled/file.cpp
+++ Script/precompiled/file.cpp
@@ -1,5 +1,15 @@
 #include <bits/stdc++.h>
 
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(), (v).end()
+
 using namespace std;
 
-int main() {}
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+}
--- Script/precompiled/gen.sh
+++ Script/precompiled/gen.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-x=$(g++ -DLOCAL -static -O2 --std=c++17 $1 -H 2>&1 | grep bits/stdc++.h)
+rm -rf bits
+
+x=$(g++ -DLOCAL -static -O2 --std=c++17 file.cpp -H 2>&1 | grep bits/stdc++.h)
 directory=`echo $x | awk '{ print $NF }'`
 
 mkdir bits
 cp $directory bits/
 cd bits
 g++ -DLOCAL -static -O2 --std=c++17 stdc++.h
+
+mv stdc++.h.gch ../../
+
+cd ..
+
+rm -rf a.out
+rm -rf bits

d1ba60b71517eb82d06ff5c636e33cd784455d25
Duy Pham <cunbidun@gmail.com>
2022-12-05 21:58:28 -0600

    Update project_config

--- task/D - Squares/solution.cpp
+++ task/D - Squares/solution.cpp
@@ -1,6 +1,6 @@
 /**
  *  author: cunbidun
- *  created: Sunday, 2022-11-27 15:44:17 CST
+ *  created: Monday, 2022-12-05 21:55:17 CST
  **/
 #include <array>
 #include <functional>
@@ -9,18 +9,7 @@
 
 using namespace std;
 
-const int MOD = 1e9 + 7;
-
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
-  int T;
-  cin >> T;
-  while (T--) {
-    long long N, A, B;
-    cin >> N >> A >> B;
-    if (A < B) {
-      swap(A, B);
-    }
-  }
 }
